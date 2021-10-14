4103690e182912fcf329a5f980b73b75c3d778bb
Akbar Irwanda <irwandaa7@gmail.com>
2021-10-14 09:03:33 +0700

    Completing the function
    Melengkapi semua fungsi agar berjalan sesuai kalkulator biasanya.

--- script.js
+++ script.js
@@ -9,28 +9,80 @@ class Calculator {
         this.previousOperand = ''
         this.currentOperand = ''
         this.operator = undefined
-        this.updateDisplay()
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number){
+        if(number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number
     }
 
     operatorFunction(operator){
-
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operator = operator
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute(){
+        let result
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch(this.operator){
+            case '+':
+                result = prev + current
+                break
+            case '-':
+                result = prev - current
+                break
+            case '*':
+                result = prev * current
+                break
+            case '÷':
+                result = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = result
+        this.operator = undefined
+        this.previousOperand = ''
+    }
 
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        }
+        else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }
+        else {
+            return integerDisplay
+        }
     }
 
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        console.log(this.currentOperand)
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operator != undefined){
+            this.previousOperandTextElement.innerText = this.getDisplayNumber(this.previousOperand) + ' ' + this.operator
+        }
+        else {
+            this.previousOperandTextElement.innerText = this.getDisplayNumber(this.previousOperand)
+        }
     }
 }
 
@@ -51,6 +103,24 @@ numberButtons.forEach(button => {
     })
 })
 
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.operatorFunction(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
 allClearButton.addEventListener('click', () => {
     calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute()
+    calculator.updateDisplay()
 })
