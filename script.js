79dffa1b7cb20c41555f5c1a3bbe74cebbd9fd72
Akbar Irwanda <irwandaa7@gmail.com>
2021-10-15 08:53:40 +0700

    add light mode
    Menambahkan light mode dengan switch menggunakan javascript

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ class Calculator {
 
     appendNumber(number){
         if(number === '.' && this.currentOperand.includes('.')) return
-        this.currentOperand = this.currentOperand.toString() + number
+        if(this.currentOperand.length < 16)
+            this.currentOperand = this.currentOperand.toString() + number
     }
 
     operatorFunction(operator){
@@ -134,3 +135,91 @@ equalsButton.addEventListener('click', () => {
     calculator.compute()
     calculator.updateDisplay()
 })
+
+const sun = document.querySelector('.fa-sun')
+const moon = document.querySelector('.fa-moon')
+var darkMode = true
+updateMode()
+
+sun.addEventListener('click', () => {
+    if(darkMode){
+        darkMode = false
+        sun.classList.remove('inactive')
+        moon.classList.add('inactive')
+        updateMode()
+    }
+
+})
+
+moon.addEventListener('click', () => {
+    if(!darkMode){
+        darkMode = true
+        moon.classList.remove('inactive')
+        sun.classList.add('inactive')
+        updateMode()
+    }
+})
+
+function updateMode(){
+    const body = document.querySelector('body')
+    const container = document.querySelector('.container')
+    const switchArea = document.querySelector('.switch')
+    const switch_i = document.querySelector('.switch i')
+    const button = document.querySelector('.button')
+    const button_button = document.querySelectorAll('.button > button')
+    const notNum = document.querySelectorAll('[notNum]')
+    const number = document.querySelectorAll('[number]')
+    const prev = document.querySelector('.output .previous-operand')
+    const cur = document.querySelector('.output .current')
+
+    if(darkMode) {
+        body.style.backgroundColor = 'darkslategray'
+        container.style.backgroundColor = 'var(--primary)'
+        switchArea.style.backgroundColor = 'var(--secondary)'
+        switch_i.style.color = 'var(--granny-smith)'
+        sun.style.color = 'var(--granny-smith)'
+        moon.style.color = 'var(--white-text)'
+        button.style.backgroundColor = 'var(--secondary)'
+        
+        button_button.forEach(btn => {
+            btn.style.backgroundColor = 'var(--secondary)'
+            btn.style.border = '1px solid var(--secondary)'
+            btn.classList.add('darkMode')
+            btn.classList.remove('lightMode')
+        })
+        
+        notNum.forEach(btn => {
+            btn.style.color = 'var(--turquoise)'
+        })
+        number.forEach(btn => {
+            btn.style.color = 'var(--white-text)'
+        })
+        prev.style.color = 'rgba(255, 255, 255, .75)'
+        cur.style.color = 'rgba(255, 255, 255, 1)'
+    }
+    else {
+        body.style.backgroundColor = 'var(--granny-smith)'
+        container.style.backgroundColor = 'white'
+        switchArea.style.backgroundColor = '#f5f5f5'
+        switch_i.style.color = 'var(--granny-smith)'
+        sun.style.color = 'var(--primary)'
+        moon.style.color = 'var(--granny-smith)'
+        button.style.backgroundColor = '#f5f5f5'
+
+        button_button.forEach(btn => {
+            btn.style.backgroundColor = '#f5f5f5'
+            btn.style.border = '1px solid #f5f5f5'
+            btn.classList.remove('darkMode')
+            btn.classList.add('lightMode')
+        })
+        notNum.forEach(btn => {
+            btn.style.color = 'var(--turquoise)'
+        })
+        number.forEach(btn => {
+            btn.style.color = 'var(--primary)'
+        })
+
+        prev.style.color = 'var(--granny-smith)'
+        cur.style.color = 'var(--primary)'
+    }
+}
